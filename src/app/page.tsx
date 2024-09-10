53703ec92e84c70299ddf57c972120847343f969
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-10 19:02:58 +0600

    feat: home page design done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+
+const LINKS = [
+  {
+    label: "Recusive Partitioning",
+    href: "/recursive-partitioning",
+  },
+  {
+    label: "Alphabet Tile Interaction",
+    href: "/alphabet-tile-interaction",
+  },
+];
 
 const HomePage = () => {
   return (
-    <section>
-      <h1>This is Home Page</h1>
+    <section className="h-[calc(100vh-56px)] w-full grid place-items-center">
+      <div className="flex flex-col gap-4">
+        {LINKS.map(({ href, label }, i) => (
+          <div
+            key={i}
+            className="flex items-center gap-2 text-xl font-medium"
+          >
+            <p>{i + 1}. </p>
+            <Link
+              href={href}
+              className="underline underline-offset-[3px] decoration-zinc-500"
+            >
+              {label}
+            </Link>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
